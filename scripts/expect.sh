16090ccd8c745c693dea3249689f2f1134983e85
useradmin007 <43361354+useradmin007@users.noreply.github.com>
2020-07-21 16:50:19 +0800

    Update expect.sh

--- scripts/expect.sh
+++ scripts/expect.sh
@@ -1,37 +1,23 @@
-#!/bin/bash
-key_file='/root/.ssh/id_rsa.pub'
-file_path='/opt/scripts/file'
-####################### 判断秘钥文件是否存在 ######################
-[ -f $key_file ] || ssh-keygen -t rsa -C '563980025@qq.com' > /dev/null 2>&1
-
-if [ -n /usr/bin/expect ];then
-	 yum install -y expect
-	   if [ $? -ne 0 ];then
-		echo "expect install bad"
-		exit $?
-	   fi
-fi
-
-####################### 批量分发秘钥 #########################
-cat $file_path/serverlist.list |while read line
+USER=root
+PASSWD=1233
+# yum install -y expect
+for HOST in 192.168.1.{1..10}
 do
-/usr/bin/expect  << EOF  > /dev/null 2>&1	
-spawn  ssh-copy-id -i $key_file  root@${line}
+        echo "------------------>" $HOST "-----------------------------"
+/usr/bin/expect -c "
+spawn ssh-copy-id $USER@$HOST;
 expect {
-        "yes/no"        {send "yes\r";exp_continue}
-        "*password"     {send "111111\r"}
-}
+            \"Are you sure you want to continue connecting (yes/no)?\" {
+            send \"yes\r\"
+            expect \"*password:\"
+            send \"$PASSWD\r\"
+            }
+            \"*password:\" {
+            send \"$PASSWD\r\"
+            }
+            \"Now try logging into the machine\" {
+            }
+        }
 expect eof
-EOF
-
-####################### 显示分发状态 ##########################
-if [ $? -eq 0 ]
-        then
-echo "${ip} 秘钥分发成功"
-#echo 0
-        else
-echo "${ip} 秘钥分发失败"
-#echo 1
-fi
+"
 done
-
